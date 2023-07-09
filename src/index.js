321c4a6ca95f64ff696fbb2cacfe8ad598d7080f
shaman2507 <alabay174@gmail.com>
2023-07-09 19:38:53 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-const root = document.getElementById('root')
-
-
-
-ReactDOM.createRoot(root).render(App())
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
